e8a1cc6c692ba3c1b4cbab2779820b3d72eb39f9
jvjishnu <jishnu.vijayan2009@gmail.com>
2020-07-12 15:37:47 +0530

    REC-3: Category Screen

---
+++ src/components/CategoryGridTile.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native';
+
+const isAndroid = Platform.OS === 'android' ? true : false
+export const CategoryGridTile = ({title, onSelect, colour}) => {
+    let TouchableComp = (isAndroid && Platform.Version >= 21) ? TouchableNativeFeedback : TouchableOpacity
+    return (
+        <View style={styles.gridItem}>
+        <TouchableComp 
+            style={{ flex: 1 }} 
+            onPress={onSelect}>
+            <View style={{...styles.container, backgroundColor: colour}}>
+                <Text style={styles.title} numberOfLines={2}>{title}</Text>
+            </View>
+        </TouchableComp>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        height: 150,
+        margin: 10,
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    container: {
+        flex: 1,
+        borderRadius: 10,
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowRadius: 10,
+        elevation: 3,
+        padding: 15,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        textAlign: 'right'
+    }
+});
--- src/navigation/MealsNavigator.js
+++ src/navigation/MealsNavigator.js
@@ -1,11 +1,29 @@
+import { Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 import { CategoriesScreen, CategoryMealsScreen, MealDetailScreen } from '../screens';
+import { Colours } from '../constants/Colours';
 
+const isAndroid = Platform.OS === 'android' ? true : false
+const navOptions = {
+    headerStyle: {
+        backgroundColor: isAndroid ? Colours.PRIMARY : ''
+    },
+    headerTintColor: isAndroid ? 'white' : Colours.PRIMARY
+}
 const MealNavigator = createStackNavigator({
-    Categories: CategoriesScreen,
-    CategoryMeals: CategoryMealsScreen,
-    MealDetail: MealDetailScreen
+    Categories: {
+        screen: CategoriesScreen,
+        title: 'Meal Categories'
+    },
+    CategoryMeals: {
+        screen: CategoryMealsScreen
+    },
+    MealDetail: {
+        screen: MealDetailScreen
+    }
+}, {
+    defaultNavigationOptions: navOptions
 });
 
 export const MealsNavigator = createAppContainer(MealNavigator)
